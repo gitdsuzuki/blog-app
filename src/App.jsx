06d4963ec829cb55d138f4c6613565f6720ca454
Daichi Suzuki <genshiroku@gmail.com>
2025-04-15 09:00:13 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="text-[100px]">
+      Hello World
+    </div>
+  )
+}
+
+export default App
