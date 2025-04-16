e1ca80311cb0eaaa51a1bf4fa221322ccd19b7a1
Daichi Suzuki <genshiroku@gmail.com>
2025-04-16 16:54:50 +0900

    課題3の実装

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,41 +1,56 @@
 import Dayjs from 'dayjs';
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { posts } from '../data/posts.js'
-import PostNotFound from './PostNotFound.jsx';
+import PostNotFound from './PostNotFound';
 
 const PostDetails = () => {
   
   const { id } = useParams()
-  const post = posts.find((elem) => elem.id == id)
+  const [post, setPosts] = useState({})
+  const [loading, setLoading] = useState(true)
 
-  if (!post) return <PostNotFound /> 
-
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`)
+      const data = await res.json()
+      setPosts(data.post)
+      setLoading(false)
+    }
+    fetcher()
+  }, [])
+  
+  if (!post) return <PostNotFound />
+  
   const { title, thumbnailUrl, createdAt, categories, content } = post
 
   return (
-    <div className="w-full pt-14 px-4">
-      <div className="mx-auto max-w-3xl">
-        <div className="pb-4">
-          <img src={thumbnailUrl} />
-        </div>
-        <div className="flex pl-3 pr-6">
-          <div className="text-sm opacity-50 flex flex-auto justify-start">
-            {Dayjs(createdAt).format("YYYY/M/DD")}
+    <>
+      { loading ? <p>読み込み中です...</p> : 
+        <div className="w-full pt-14 px-4">
+          <div className="mx-auto max-w-3xl">
+            <div className="pb-4">
+              <img src={thumbnailUrl} />
+            </div>
+            <div className="flex pl-3 pr-6">
+              <div className="text-sm opacity-50 flex flex-auto justify-start">
+                {Dayjs(createdAt).format("YYYY/M/DD")}
+              </div>
+              <div className="flex flex-auto justify-end">
+                <ul className="flex text-sm">
+                  {categories.map((elem) => 
+                  <li key={elem} className="text-sky-700 border-1 py-1 px-1.5 ml-2 rounded-sm">{elem}</li>
+                  )}
+                </ul>
+              </div>
+            </div>
+            <div className="text-2xl pl-3 py-2 mb-2">
+                {title}
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: content }} className="pl-3 opacity-80" />
           </div>
-          <div className="flex flex-auto justify-end">
-            <ul className="flex text-sm">
-              {categories.map((elem) => 
-              <li key={elem} className="text-sky-700 border-1 py-1 px-1.5 ml-2 rounded-sm">{elem}</li>
-              )}
-            </ul>
-          </div>
         </div>
-        <div className="text-2xl pl-3 py-2 mb-2">
-            {title}
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: content }} className="pl-3 opacity-80" />
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
